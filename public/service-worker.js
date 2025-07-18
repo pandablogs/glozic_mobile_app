0c80728924225244360d3fc653a07db107029ca3
Anup Maurya <anup.maurya484@gmail.com>
2025-07-18 11:44:07 +0530

    change

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,32 +1,8 @@
-// self.addEventListener("install", (e) => {
-//   self.skipWaiting();
-// });
-
-// self.addEventListener("fetch", (e) => {
-//   e.respondWith(fetch(e.request));
-// });
-
-
-
-const CACHE_NAME = "v1";
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/image/icon-192.png',
-  '/image/icon-512.png'
-];
-
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (e) => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
-  );
+self.addEventListener("fetch", (e) => {
+  e.respondWith(fetch(e.request));
 });
+
