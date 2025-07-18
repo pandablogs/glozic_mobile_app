442742de140ef4dae5f04d5b5951afe544f63f6d
Anup Maurya <anup.maurya484@gmail.com>
2025-07-18 15:06:19 +0530

    mobile set

---
+++ public/service-worker.js
@@ -0,0 +1,22 @@
+const cacheName = 'v1';
+const cacheAssets = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(cacheAssets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,16 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter as Router } from 'react-router-dom'
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered:', reg.scope))
+      .catch(err => console.error('Service Worker error:', err));
+  });
+}
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
